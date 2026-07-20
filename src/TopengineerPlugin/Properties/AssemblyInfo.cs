55260ca0176360314e6790b1136b7b24aa129dee
BOK-stack <bazarbaeomar@gmail.com>
2026-07-20 23:55:17 +0300

    Add assembly info

---
+++ src/TopengineerPlugin/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Topengineer Plugin for Tekla Structures")]
+[assembly: AssemblyDescription("Comprehensive plugin for automation of metal structure design according to GOST standards")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("BOK-stack")]
+[assembly: AssemblyProduct("TopengineerPlugin")]
+[assembly: AssemblyCopyright("Copyright © 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("12345678-1234-1234-1234-123456789012")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
